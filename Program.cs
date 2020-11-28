1850d3f60aff77e0d392e98bec96d5cdebab4fdc
Sam W <twopizza9621536@gmail.com>
2020-11-28 17:26:38 -0500

    Adding MSLearn tutorial

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace files_module
 {
@@ -6,7 +9,62 @@ namespace files_module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+            var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+            Directory.CreateDirectory(salesTotalDir);
+
+            var salesFiles = FindFiles(storesDirectory);
+
+            var salesTotal = CalculateSalesTotal(salesFiles);
+
+            File.WriteAllText(Path.Combine(salesTotalDir, "totales.txt"), $"{salesTotal}{Environment.NewLine}");
         }
+
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                var extension = Path.GetExtension(file);
+
+                // The file name will contain the full path, so only check the end of it
+                if (extension == ".json")
+                {
+                    salesFiles.Add(file);
+                }
+            }
+
+            return salesFiles;
+        }
+
+        static double CalculateSalesTotal(IEnumerable<string> salesFiles)
+        {
+            double salesTotal = 0;
+
+            // Loop over each file path on salesFiles
+            foreach (var file in salesFiles)
+            {
+                // Read the contents of the file
+                string salesJson = File.ReadAllText(file);
+
+                // Parse the contents as JSON
+                SalesData data = JsonConvert.DeserializeObject<SalesData>(salesJson);
+
+                // Add the amount found in the Total field to the salesTotal variable
+                salesTotal += data.Total;
+            }
+
+            return salesTotal;
+        }
+    }
+
+    class SalesData
+    {
+        public double Total { get; set; }
     }
 }
